e40ccfd9588f891eaa83e735c36bf2f17397bc6e
Kerwin Mayorga <kerwinmayorga@gmail.com>
2021-08-06 00:04:42 -0400

    suped up the homepage as well as the about us page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,9 @@ class HomeController < ApplicationController
   end
 
   def about
-  	@about_me = "My name is K, this is a little about me."
-  	@answer = 2 + 2
+    @about_me1 = "Self taught and self proclaimed! I am Kerwin, a student of the arts. I enjoy developing and designing websites, so I code using HTML, CSS, JS, PHP, and Ruby."
+  	@about_me2 = "This is my first Ruby built application. The framework I used to build this app is Rails. I used Bootstrap and CSS to design the app. And HTML and Javascript to add content."
+    @enjoy = "I hope you enjoy!"
   end
   
 end
